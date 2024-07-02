76574446e2794de2d6ff5097b1d5e2321c26e188
Lucas Tonelotto <lucasfrantone@gmail.com>
2024-07-02 09:16:10 -0300

    chore: Update dependencies and add missing configuration files for npm packages

--- server/index.js
+++ server/index.js
@@ -1,13 +1,15 @@
 import express from 'express'
+import logger from 'morgan'
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+    res.sendFile(process.cwd() + '/client/index.html')
 })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
-})
+}) 
