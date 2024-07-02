49c9d4d3be2772665b82d001b6cb14bf880524a9
Lucas Tonelotto <lucasfrantone@gmail.com>
2024-07-02 09:21:49 -0300

    chore: Update dependencies and add socket.io for real-time communication

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 import express from 'express'
 import logger from 'morgan'
 
+import { Server } from 'socket.io'
+import { createServer } from 'http'
+
 const port = process.env.PORT ?? 3000
 
 const app = express()
+const server = createServer(app)
+const io = new Server(server)
+
+io.on('connection', () => {
+    console.log('user connected')
+})
+
 app.use(logger('dev'))
 
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`)
 }) 
